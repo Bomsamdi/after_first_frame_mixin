ae8881d25f542125543229c0cb2812d8177941fa
Bartosz Wróbel <bwrobel@everpress.pl>
2022-10-18 20:46:39 +0200

    Init commit

---
+++ example/lib/main.dart
@@ -0,0 +1,64 @@
+import 'dart:async';
+
+import 'package:after_first_frame_mixin/after_first_frame_mixin.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+@immutable
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Example',
+      home: HomeScreen(),
+    );
+  }
+}
+
+@immutable
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  HomeScreenState createState() => HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen>
+    with AfterFirstFrameMixin<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: const Center(
+          child: Text("Example"),
+        ),
+      ),
+    );
+  }
+
+  void showHelloWorld() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: const Text('Hello World'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('DISMISS'),
+            )
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  FutureOr<void> afterFirstFrame(BuildContext context) {
+    // Calling the same function "after first frame" to resolve the issue.
+    showHelloWorld();
+  }
+}
